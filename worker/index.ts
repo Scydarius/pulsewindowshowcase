6c9f47bf798bddd381748e381ed195951f979756
sharms <sharms@pedarecc.sa.edu.au>
2026-09-01 12:26:25 +0930

    Add vinext worker handler

--- worker/index.ts
+++ worker/index.ts
@@ -1,1 +1,7 @@
-export default {};
+import handler from "vinext/server/app-router-entry";
+
+export default {
+  fetch(request: Request, env: unknown, ctx: ExecutionContext) {
+    return handler.fetch(request, env, ctx);
+  },
+};
